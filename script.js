3e24ccf6702e4bab6a90c28d41160ee2dcfb016b
Jasmine Daniels <create.jasminedaniels@gmail.com>
2024-03-06 17:47:03 -0500

    toggle click event for lightbulbs

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Write your code here
+let lightBulbs = document.querySelectorAll('.lightbulb');
+let subtitle = document.querySelector('.subtitle');
+
+let count = 0;
+
+lightBulbs.forEach((bulb) => {
+    bulb.addEventListener('click', function(){
+        count += 1;
+        subtitle.innerHTML = `You've clicked the lights ${count} times`
+        bulb.classList.toggle('active');
+    })
+});
